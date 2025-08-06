b9670114cc751167c896d461719d61e2c9981ead
Shivam Bhalekar <shivambhalekar37@gmail.com>
2025-08-06 16:20:09 +0530

    modified

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main(){
     cout<<"Hello World!"<<endl;
-    cout<<"Hello World!"<<endl;
-    cout<<"Hello World!"<<endl;
+    cout<<"Hello everyone!"<<endl;
+    cout<<"Hello to friends!"<<endl;
+    cout<<"Currently learning Git and Github"<<endl;
     return 0;
 }
