d783d67e1a08d953a4872096e83a0666893022bd
Atharva Patil <patilatharva021206@gmail.com>
2025-08-06 16:53:04 +0530

    Modified Shivam code

--- hello.cpp
+++ hello.cpp
@@ -7,5 +7,6 @@ int main(){
     cout<<"Hello to friends!"<<endl;
     cout<<"Currently learning Git and Github"<<endl;
     cout<<"Currently learning Branches in git"<<endl;
+    cout<<"I am Atharva"<<endl;
     return 0;
 }
